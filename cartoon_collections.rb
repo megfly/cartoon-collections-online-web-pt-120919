1bef6a27a76d65470b44c4f5d21495a9ec6d0aca
Meghan Flynn <mflynn333@gmail.com>
2019-12-18 00:47:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? > do |word|
-    word.length > 4 
-  end 
+ end 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
