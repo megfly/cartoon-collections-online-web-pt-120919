438354bf3b56dd032f74a82588c1cde2b216177c
Meghan Flynn <mflynn333@gmail.com>
2019-12-18 00:50:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,9 @@ def long_planeteer_calls(array)
   end 
 end 
 
-def find_the_cheese
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.find do |cheese|
+    cheese.include 
 end
