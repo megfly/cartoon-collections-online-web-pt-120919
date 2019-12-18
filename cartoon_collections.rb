ec5fb75fb4740a0b7f5a865ea135be90454abfe7
Meghan Flynn <mflynn333@gmail.com>
2019-12-18 00:39:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  new_array = []
-  
-  array.map do |summons| summons.capitalize
+  array.collect do |summons| summons.capitalize
     
 end 
 end
